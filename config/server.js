a4963137a93c81a734397f7883ec7f36016ec591
Resit Korsu <resitkorsu@gmail.com>
2021-04-18 13:43:50 +0300

    initial todo api

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '264bf94cf4883cb629d738266acb9f26'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ecff299c-0df9-4ccc-bb71-036bfbe46a8d'
+};
